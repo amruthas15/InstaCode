935a4a44d76242ab2325c9ce6dd62615c7e2a0bc
Amrutha Srikanth <60552165+amruthas15@users.noreply.github.com>
2021-07-06 13:55:55 -0700

    Initial Commit with Parse and Backend Setup

--- InstaCode/AppDelegate.m
+++ InstaCode/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -14,11 +15,19 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(  NSDictionary *)launchOptions {
+
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+        configuration.applicationId = @"XVSW7OFsj6YwjvkzOZ6aE2eOGzNEdoUjly9JoFhX"; // <- UPDATE
+        configuration.clientKey = @"syAa7gtBbQXjN7LtaUEgLK9vecqxshFjA3ff4BIe"; // <- UPDATE
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+
     return YES;
 }
-
 
 #pragma mark - UISceneSession lifecycle
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'InstaCode' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for InstaCode
+  pod 'Parse'
+
+  target 'InstaCodeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'InstaCodeUITests' do
+    # Pods for testing
+  end
+
+end
